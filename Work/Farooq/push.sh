74a9b4efbfec7f2e331cbd29978529b7508ce560
mfarooq786 <2714befarooq@gmail.com>
2023-10-12 18:35:54 -0500

    change

--- Work/Farooq/push.sh
+++ Work/Farooq/push.sh
@@ -1,13 +1,12 @@
-cd code_playground/DNN-CFFs/Work/Farooq
+cd Github/DNN-CFFs
 git config --global user.name "mfarooq786"
 git config --global user.email "2714befarooq@gmail.com"
 ssh-keygen -t ed25519 -C "2714befarooq@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-git remote set-url origin git@github.com:mfarooq786/DNN-CFFs.git
-#git branch 
+git remote set-url origin git@github.com:uva-spin/DNN-CFFs.git
 git status
-#git add . 
-git add --a
+git add . 
+git add —a
 git commit -m "change"
-git push 
+git push origin main
--- Work/Farooq/script1.sh
+++ Work/Farooq/script1.sh
@@ -2,17 +2,17 @@
 
 # Initialize conda for the Bash shell (if not already initialized)
 # Make sure to replace 'your-conda-environment' with the name of your conda environment
-#conda init bash
-#source ~/.bashrc
+conda init bash
+source ~/.bashrc
 
 # Activate your conda environment
-#conda activate tf_environment
+conda activate tf_environment
 
 # Change to the directory where your Python script is located
-cd Github/DNN-CFFs/Work/Farooq
+cd code_playground/
 
 # Run your Python script (replace 'script.py' with your script name)
-python training.py
+python code1.py
 
 # Deactivate the conda environment (optional)
 conda deactivate
