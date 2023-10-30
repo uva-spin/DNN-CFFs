c90a68c4a91ecda972c5f3e94b50514869633291
nuwangw <mra2mh@virginia.edu>
2023-10-30 16:39:22 -0500

    adding Nuwan directory

---
+++ Work/Nuwan/non_model_utils.py
@@ -0,0 +1,22 @@
+class F1F2:
+    def __init__(self):
+        self.GM0 = 2.792847337
+
+    def ffGE(self, t):
+        GE = 1.0 / (1.0 + (-t / 0.710649)) / (1.0 + (-t / 0.710649))
+        return GE
+
+    def ffGM(self, t):
+        shape = self.ffGE(t)
+        return self.GM0 * shape
+
+    def ffF2(self, t):
+        f2 = (self.ffGM(t) - self.ffGE(t)) / (1.0 - t / (4.0 * 0.938272 * 0.938272))
+        return f2
+
+    def ffF1(self, t):
+        f1 = self.ffGM(t) - self.ffF2(t)
+        return f1
+
+    def f1_f21(self, t):
+        return self.ffF1(t), self.ffF2(t)
