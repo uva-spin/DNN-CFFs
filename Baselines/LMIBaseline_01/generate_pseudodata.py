6d3e7c09ac7577c833dd3abf88c4637c09d7599f
ChinmayGowda71 <chinmay.gow2@gmail.com>
2023-10-30 20:44:01 -0400

    baselines added

---
+++ Baselines/LMIBaseline_01/generate_pseudodata.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+
+from non_model_utils import F1F2
+from model_utils import F_calc
+
+file_name = 'data.csv'
+
+df = pd.read_csv(file_name, dtype=np.float32)
+df.set_index('index')
+
+fns = F1F2()
+calc = F_calc()
+
+interval = 18
+ReH = 1
+ReE = 1
+ReHtilde = 1
+dvcs = 1
+#temp_ReH = np.array([ReH + 0 * i for i in range(0, len(df["ReH"]))])
+#temp_ReE = np.array([ReE + 0 * i for i in range(0, len(df["ReE"]))])
+#temp_ReHtilde = np.array([ReHtilde + 0 * i for i in range(0, len(df["ReHtilde"]))])
+#temp_dvcs = np.array([dvcs + 0 * i for i in range(0, len(df["dvcs"]))])
+for i in range(len(df)):
+    row = df.loc[i]
+    kin_ranges = []  # QQ, x_b, t, k
+    for kin in [row['QQ'], row['x_b'], row['t'], row['k']]:
+        low = kin * .95
+        high = kin * 1.05
+        kin_ranges.append(np.linspace(
+            min(low, high), max(low, high), 3, dtype=np.float32))
+    rows = []
+    # starts at interval, ends at 360 (inclusive)
+    for phi in range(interval, 361, interval):
+        f_calcs = []
+        for QQ in kin_ranges[0]:
+            for x_b in kin_ranges[1]:
+                for t in kin_ranges[2]:
+                    F1, F2 = fns.f1_f21(t)
+                    for k in kin_ranges[3]:
+                       #       f_calcs.append(calc.fn_1([phi, QQ, x_b, t, k, F1, F2], [row["ReH"], row['ReE'], row['ReHtilde'], row['dvcs']]))
+                        f_calcs.append(calc.fn_1(
+                            [phi, QQ, x_b, t, k, F1, F2], [ReH, ReE, ReHtilde, dvcs]))
+        f_calcs = np.array(f_calcs)
+        rows.append([int(row['index']), row['k'], row['QQ'], row['x_b'], row['t'], phi, np.mean(
+            f_calcs), np.std(f_calcs), ReH, ReE, ReHtilde, dvcs])
+        print(i, phi)
+
+    new_df = pd.DataFrame(rows, columns=[
+                          'experiment', 'k', 'QQ', 'x_b', 't', 'phi_x', 'F', 'sigmaF', 'ReH', 'ReE', 'ReHtilde', 'dvcs'])
+
+    new_df.to_csv(f'BKM_pseudodata_{i}.csv', index_label='index')
