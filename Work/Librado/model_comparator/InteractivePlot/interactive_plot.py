a6aadc41e652dc509f27ea9036879bb969ace57d
lba9wf <51801672+lba9wf@users.noreply.github.com>
2023-12-27 14:18:47 +0530

    Update interactive_plot.py

--- Work/Librado/model_comparator/InteractivePlot/interactive_plot.py
+++ Work/Librado/model_comparator/InteractivePlot/interactive_plot.py
@@ -20,41 +20,48 @@ def average_predictions(folder_path, pseudodata_path):
     merged_df.to_csv('average_predictions.csv', index=False)
     return 'average_predictions.csv'
 
-def create_interactive_3d_plot(csv_file, x_col, y_col, z_col):
+def create_interactive_plot(csv_file, x_col, y_col, z_col=None, cff_slices=None):
     df = pd.read_csv(csv_file)
-    fig = px.scatter_3d(df, x=x_col, y=y_col, z=z_col, title=f'3D Plot of {y_col} with {x_col} and {z_col}')
+    if cff_slices:
+        # Dynamically create the query condition based on y-value
+        query_conditions = []
+        for cff_name, cff_range in cff_slices.items():
+            if cff_name != y_col:
+                query_conditions.append(f"{cff_name} >= @cff_slices['{cff_name}'][0] and {cff_name} <= @cff_slices['{cff_name}'][1]")
+        query_string = " and ".join(query_conditions)
+        df = df.query(query_string)
+
+    if z_col:
+        fig = px.scatter_3d(df, x=x_col, y=y_col, z=z_col, title=f'3D Plot of {y_col} with {x_col} and {z_col}')
+    else:
+        fig = px.scatter(df, x=x_col, y=y_col, title=f'2D Plot of {y_col} vs {x_col}')
     fig.show()
 
-def create_interactive_2d_plot(csv_file, x_col, y_col):
-    df = pd.read_csv(csv_file)
-    fig = px.scatter(df, x=x_col, y=y_col, title=f'2D Plot of {y_col} vs {x_col}')
-    fig.show()
+def get_user_input():
+    z_col = 0
+    print("Do you want a 2D or 3D plot?")
+    plot_type = input("Enter '2d' or '3d': ").lower()
+    x_col = input("Enter the x-axis column: ")
+    y_col = input("Enter the y-axis column: ")
+    if plot_type == '3d':
+        z_col = input("Enter the z-axis column: ")
+
+    use_default_cff_ranges = input("Would you like to use default ranges for other CFF values (y/n)? ").lower() == 'y'
+    cff_slices = {}
+    if not use_default_cff_ranges:
+        # Only prompt for ranges of CFFs other than the y-value
+        for cff_name in ["ReH", "ReE", "ReHt"]:
+            if cff_name != y_col:
+                cff_slices[cff_name] = tuple(float(val) for val in input(f"Enter {cff_name} range (min, max): ").split(','))
+    return plot_type, x_col, y_col, z_col, cff_slices
+
+def main():
+    try:
+        plot_type, x_col, y_col, z_col, cff_slices = get_user_input()
+        avg_csv = average_predictions('../DNNvalues', '../PseudoData_from_the_Basic_Model.csv')
+        create_interactive_plot(avg_csv, x_col, y_col, z_col, cff_slices)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python interactive_plot.py <plot_type> <x_column> <y_column> [<z_column>]")
-        sys.exit(1)
-
-    plot_type = sys.argv[1]
-    x_col = sys.argv[2]
-    y_col = sys.argv[3]
-    z_col = sys.argv[4] if len(sys.argv) == 5 else None
-
-    valid_columns = ['F', 'ReH', 'ReE', 'ReHt', 'dvcs', 'QQ', 'x_b', 't', 'phi_x', 'k']
-    if x_col not in valid_columns or y_col not in valid_columns or (z_col and z_col not in valid_columns):
-        print("Invalid columns specified. Please choose from the specified columns.")
-        sys.exit(1)
-
-    try:
-        avg_csv = average_predictions('../DNNvalues', '../PseudoData_from_the_Basic_Model.csv')
-        if plot_type == '2d':
-            create_interactive_2d_plot(avg_csv, x_col, y_col)
-        elif plot_type == '3d':
-            if not z_col:
-                print("Z column required for 3D plot.")
-                sys.exit(1)
-            create_interactive_3d_plot(avg_csv, x_col, y_col, z_col)
-        else:
-            print("Invalid plot type. Choose '2d' or '3d'.")
-    except Exception as e:
-        print(str(e))
+    main()
