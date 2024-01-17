950959a28cd15ec6a9af0ec71668838ffaf51cbf
isharafernando <isharafernando@gmail.com>
2024-01-16 18:05:41 -0600

    Added sample set of codes to get started

---
+++ Work/Ishara/Sample_Codes_v1/Sample_Codes_to_Get_Started/Example_LocalFit_Code/Sample_Local_Fit.py
@@ -0,0 +1,242 @@
+##########################################################################
+###############  Written by Ishara Fernando                  #############
+##############  Revised Date: 01/16/2024    ##############################
+#####  This code is for testing on a local computer or on a single node ##
+##### First: Run the following commands on your Rivanna terminal #########
+## source /home/lba9wf/miniconda3/etc/profile.d/conda.sh         #########
+## conda activate env                                            #########
+##########################################################################
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from BHDVCS_tf_modified import *
+import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import os
+import sys
+
+
+def create_folders(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        print(f"Folder '{folder_name}' created successfully!")
+    else:
+        print(f"Folder '{folder_name}' already exists!")
+        
+
+create_folders('DNNmodels')
+create_folders('Losses_CSVs')
+create_folders('Losses_Plots')
+create_folders('Replica_Data')
+create_folders('Replica_Results')
+
+data_file = 'Sample_Local_PseudoData_from_the_Basic_Model_for_JLab_Kinematics.csv'
+df = pd.read_csv(data_file, dtype=np.float64)
+df = df.rename(columns={"sigmaF": "errF"})
+
+
+
+#### User's inputs ####
+Learning_Rate = 0.0001
+EPOCHS = 100
+EarlyStop_patience = 1000
+modify_LR_patience = 400
+modify_LR_factor = 0.9
+
+NUM_REPLICAS = 2
+
+modify_LR = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',factor=modify_LR_factor,patience=modify_LR_patience,mode='auto')
+EarlyStop = tf.keras.callbacks.EarlyStopping(monitor='loss',patience=EarlyStop_patience)
+
+
+def chisquare(y, yhat, err):
+    return np.sum(((y - yhat)/err)**2)
+
+def split_data(X,y,yerr,split=0.1):
+  temp =np.random.choice(list(range(len(y))), size=int(len(y)*split), replace = False)
+
+  test_X = pd.DataFrame.from_dict({k: v[temp] for k,v in X.items()})
+  train_X = pd.DataFrame.from_dict({k: v.drop(temp) for k,v in X.items()})
+
+  test_y = y[temp]
+  train_y = y.drop(temp)
+
+  test_yerr = yerr[temp]
+  train_yerr = yerr.drop(temp)
+
+  return train_X, test_X, train_y, test_y, train_yerr, test_yerr
+
+
+####### Here we define a function that can sample F within sigmaF ###
+def GenerateReplicaData(df):
+    pseudodata_df = {'k': [],
+                     'QQ': [],
+                     'x_b': [],
+                     't': [],
+                     'phi_x': [],
+                     'F':[],
+                     'errF':[]}
+    #pseudodata_df = pd.DataFrame(pseudodata_df)
+    pseudodata_df['k'] = df['k']
+    pseudodata_df['QQ'] = df['QQ']
+    pseudodata_df['x_b'] = df['x_b']
+    pseudodata_df['t'] = df['t']
+    pseudodata_df['phi_x']= df['phi_x']
+    pseudodata_df['errF']= df['errF']
+    pseudodata_df['dvcs']= df['dvcs']
+    tempF = np.array(df['F'])
+    tempFerr = np.abs(np.array(df['errF'])) ## Had to do abs due to a run-time error
+    ReplicaF = np.random.normal(loc=tempF, scale=tempFerr)
+    pseudodata_df['F']=ReplicaF
+    return pd.DataFrame(pseudodata_df)
+
+
+
+def DNNmodel():
+    initializer = tf.keras.initializers.RandomUniform(minval=-0.1,maxval=0.1,seed=None)
+    #### QQ, x_b, t, phi, k ####
+    inputs = tf.keras.Input(shape=(5), name='input_layer')
+    QQ, x_b, t, phi, k = tf.split(inputs, num_or_size_splits=5, axis=1)
+    kinematics = tf.keras.layers.concatenate([QQ, x_b, t])
+    x1 = tf.keras.layers.Dense(100, activation="tanh", kernel_initializer=initializer)(kinematics)
+    x2 = tf.keras.layers.Dense(100, activation="tanh", kernel_initializer=initializer)(x1)
+    outputs = tf.keras.layers.Dense(4, activation="linear", kernel_initializer=initializer, name='cff_output_layer')(x2)
+    #### QQ, x_b, t, phi, k, cffs ####
+    total_FInputs = tf.keras.layers.concatenate([inputs, outputs], axis=1)
+    TotalF = TotalFLayer(name='TotalFLayer')(total_FInputs) # get rid of f1 and f2
+    tfModel = tf.keras.Model(inputs=inputs, outputs = TotalF, name="tfmodel")
+    tfModel.compile(
+        optimizer = tf.keras.optimizers.Adam(Learning_Rate),
+        loss = tf.keras.losses.MeanSquaredError()
+    )
+    return tfModel
+
+
+
+def calc_yhat(model, X):
+    return model.predict(X)
+    
+
+def absolute_residual(tr, prd):
+    temp_diff = tr - prd
+    temp_abs_diff = np.abs(temp_diff) 
+    return temp_abs_diff
+
+
+def GenerateReplicaResults(df,model):
+    pseudodata_df = {'k': [],
+                     'QQ': [],
+                     'x_b': [],
+                     't': [],
+                     'phi_x': [],
+                     'F':[],
+                     'errF':[],                     
+                     'ReH': [],
+                     'ReE': [],
+                     'ReHt': [],
+                     'dvcs': [],
+                     'AbsRes_ReH':[],
+                     'AbsRes_ReE':[],
+                     'AbsRes_ReHt':[],
+                     'AbsRes_dvcs':[]}
+    tempX = df[['QQ', 'x_b', 't','phi_x', 'k']]
+    PredictedCFFs = np.array(tf.keras.backend.function(model.get_layer(name='input_layer').input, model.get_layer(name='cff_output_layer').output)(tempX))
+    PredictedFs = np.array(tf.keras.backend.function(model.get_layer(name='input_layer').input, model.get_layer(name='TotalFLayer').output)(tempX))
+    pseudodata_df['k'] = df['k']
+    pseudodata_df['QQ'] = df['QQ']
+    pseudodata_df['x_b'] = df['x_b']
+    pseudodata_df['t'] = df['t']
+    pseudodata_df['phi_x']= df['phi_x']
+    pseudodata_df['errF']= df['errF']
+    pseudodata_df['dvcs']= df['dvcs']
+    pseudodata_df['F']= list(PredictedFs.flatten())
+    pseudodata_df['ReH'] = list(PredictedCFFs[:, 0])
+    pseudodata_df['ReE'] = list(PredictedCFFs[:, 1])
+    pseudodata_df['ReHt'] = list(PredictedCFFs[:, 2])
+    pseudodata_df['dvcs'] = list(PredictedCFFs[:, 3])
+    pseudodata_df['AbsRes_ReH'] = list(absolute_residual(df['ReH'],pseudodata_df['ReH']))
+    pseudodata_df['AbsRes_ReE'] = list(absolute_residual(df['ReE'],pseudodata_df['ReE']))
+    pseudodata_df['AbsRes_ReHt'] = list(absolute_residual(df['ReHt'],pseudodata_df['ReHt']))
+    pseudodata_df['AbsRes_dvcs'] = list(absolute_residual(df['dvcs'],pseudodata_df['dvcs']))
+    return pd.DataFrame(pseudodata_df)
+
+
+
+
+
+def run_replica(i):
+    #replica_number = sys.argv[1]   # If you want to use this scrip for job submission, then uncomment this line, 
+    #  then comment the following line, and then delete the 'i' in the parenthesis of run_replica(i) function's definition
+    replica_number = i
+    tempdf = GenerateReplicaData(df)
+    tempdf.to_csv('Replica_Data/rep_data' + str(replica_number) + '.csv')
+
+    trainKin, testKin, trainY, testY, trainYerr, testYerr = split_data(tempdf[['QQ', 'x_b', 't', 'phi_x', 'k']],
+                                                                       tempdf['F'], tempdf['errF'], split=0.1)
+
+    tfModel = DNNmodel()
+    history = tfModel.fit(trainKin, trainY, validation_data=(testKin, testY), epochs=EPOCHS, callbacks=[modify_LR],
+                          batch_size=300, verbose=2)
+    
+    tfModel.save('DNNmodels/' + 'model' + str(replica_number) + '.h5', save_format='h5')
+
+    tempX = df[['QQ', 'x_b', 't', 'phi_x', 'k']]
+
+    PredictedCFFs = np.array(tf.keras.backend.function(tfModel.get_layer(name='input_layer').input, tfModel.get_layer(name='cff_output_layer').output)(tempX))
+    PredictedFs = np.array(tf.keras.backend.function(tfModel.get_layer(name='input_layer').input, tfModel.get_layer(name='TotalFLayer').output)(tempX))
+
+    replicaResultsdf = GenerateReplicaResults(df, tfModel)
+    replicaResultsdf.to_csv('Replica_Results/rep_result' + str(replica_number) + '.csv')
+
+    tempdf = pd.DataFrame()
+    tempdf["Train_Loss"] = history.history['loss'][-100:]
+    tempdf["Val_Loss"] = history.history['val_loss'][-100:]
+    tempdf.to_csv('Losses_CSVs/' + 'reploss_' + str(replica_number) + '.csv')
+    
+
+    # Create 4D interactive scatter plots based on accuracy ranges
+    org_file = data_file
+    rep_dat_file = 'Replica_Data/rep_data' + str(replica_number) + '.csv'
+    rep_file = 'Replica_Results/rep_result' + str(replica_number) + '.csv'
+    org_df = pd.read_csv(org_file, dtype=np.float64)
+    rep_dat_df = pd.read_csv(rep_dat_file, dtype=np.float64)
+    rep_df = pd.read_csv(rep_file, dtype=np.float64)
+    
+    org_sliced = org_df
+    rep_dat_sliced = rep_dat_df
+    rep_sliced = rep_df
+
+    # Create subplots for loss plots
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['loss'])
+    plt.title('Train Loss')
+    #plt.ylim(0,0.01)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['val_loss'])
+    plt.title('Validation Loss')
+    #plt.ylim(0,0.01)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+
+    plt.savefig('Losses_Plots/' + 'loss_plots' + str(replica_number) + '.pdf')
+    plt.close()
+
+    # Create a single figure for 4D interactive scatter plots
+    fig = plt.figure(figsize=(20, 25))
+    
+
+    
+###### Running Jobs on Rivanna: Comment the following lines and uncomment the run_replica(), uncomment replica_number = sys.argv[1] and comment replica_number = i in the 'def run_replica()'  
+for i in range(0,NUM_REPLICAS):
+    run_replica(i)
+#run_replica()
