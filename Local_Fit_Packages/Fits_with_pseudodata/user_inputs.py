aaee2abdb46cf584cba9d58ccca750e87523b101
AnirudhVenkatapuram <qzf7nj@virginia.edu>
2025-02-19 17:46:32 -0500

    Added local fit packages, updated

---
+++ Local_Fit_Packages/Fits_with_pseudodata/user_inputs.py
@@ -0,0 +1,23 @@
+# ALWAYS CHANGE PATHS
+scratch_path = '/scratch/<computing_id>/<path_of_your_choice>'
+
+#Data file
+initial_data_file = 'Pseudo_data_with_sampling.csv'
+
+#Number of sets
+kinematic_sets = list(range(100, 151))
+
+#Model parameters:
+
+Learning_Rate = 0.01
+EPOCHS = 2000
+BATCH = 20
+EarlyStop_patience = 1000
+modify_LR_patience = 400
+modify_LR_factor = 0.9
+
+
+
+
+
+
