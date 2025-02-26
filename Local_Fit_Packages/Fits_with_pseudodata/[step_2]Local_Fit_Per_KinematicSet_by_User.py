680dd74b8d8224f20b50dc9b4980fe46ab43b294
Ishara Fernando <isharafernando@gmail.com>
2025-02-26 16:01:16 -0600

    Update [step_2]Local_Fit_Per_KinematicSet_by_User.py

--- Local_Fit_Packages/Fits_with_pseudodata/[step_2]Local_Fit_Per_KinematicSet_by_User.py
+++ Local_Fit_Packages/Fits_with_pseudodata/[step_2]Local_Fit_Per_KinematicSet_by_User.py
@@ -100,7 +100,7 @@ def GenerateReplicaData(df):
     #ReplicaF = np.random.normal(loc=tempF, scale=tempFerr)
     while True:
         ReplicaF = np.random.normal(loc=tempF, scale=tempFerr)
-        if ReplicaF > 0:
+        if ReplicaF.all() > 0:
             break
     pseudodata_df['F'] = ReplicaF
     return pd.DataFrame(pseudodata_df)
