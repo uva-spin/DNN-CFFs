8a2c74bf21c17df31e6361934e797a51f3abca9d
isharafernando <isharafernando@gmail.com>
2025-08-25 16:05:28 -0500

    added Fits_with_KM15_test_kins_1-10_with_keras_models_NO_h5

---
+++ Local_Fit_Packages/Fits_with_pseudodata/Fits_with_KM15_test_kins_1-10_with_keras_models_NO_h5/user_inputs.py
@@ -0,0 +1,23 @@
+# ALWAYS CHANGE PATHS
+scratch_path = 'scratch'
+
+#Data file
+initial_data_file = 'km15_bkm10_dropped.csv'
+
+#Number of sets
+kinematic_sets = list(range(1, 10))
+
+#Model parameters:
+
+Learning_Rate = 0.01
+EPOCHS = 2000
+BATCH = 20
+EarlyStop_patience = 100
+modify_LR_patience = 400
+modify_LR_factor = 0.9
+
+
+
+
+
+
